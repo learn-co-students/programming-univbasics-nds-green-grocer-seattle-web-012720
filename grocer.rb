2524ba786e25eda8e167acbb6117230cb24643c6
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-02-24 11:24:01 -0800

    Done.

--- grocer.rb
+++ grocer.rb
@@ -10,23 +10,23 @@ end
 
 def consolidate_cart(cart)
   new_cart = []
-  counter = 0 
+  counter = 0
   while counter < cart.length 
-  new_cart_item = find_item_by_name_in_collection(cart[counter][:item],new_cart)
-  if new_cart_item != nil 
-    new_cart_item[:count] += 1 
-  else 
-    new_cart_item = {
+    new_cart_item = find_item_by_name_in_collection(cart[counter][:item],new_cart)
+    if new_cart_item != nil
+      new_cart_item[:count] += 1
+    else
+      new_cart_item = {
       :item => cart[counter] [:item],
       :price => cart[counter] [:price],
       :clearance => cart[counter] [:clearance],
       :count => 1 
-    }
+    } 
     new_cart << new_cart_item
   end
   counter += 1
- end
- new_cart
+end
+new_cart
 end
 
 def apply_coupons(cart, coupons)
